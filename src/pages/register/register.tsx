59f0d0b1eb461c02dee7ccb8668f5ae3acff06c1
Vladislav Zainullin <mittoface02@mail.ru>
2026-04-21 21:11:36 +0300

    Дорабатывает страницу Register

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,13 +1,35 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch } from '../../services/store';
+import { registerThunk } from '../../services/slices/user';
+import { TRegisterData } from '@api';
+import { NavigateOptions, useNavigate } from 'react-router-dom';
 
 export const Register: FC = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    const registerData: TRegisterData = {
+      name: userName,
+      email: email,
+      password: password
+    };
+
+    dispatch(registerThunk(registerData)).then(() => {
+      const navigateOptions: NavigateOptions = {
+        replace: true
+      };
+
+      navigate('/profile', navigateOptions);
+    });
   };
 
   return (
