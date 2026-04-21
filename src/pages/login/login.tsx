b1f990a718d809f77cff8b1166461286379a643d
Vladislav Zainullin <mittoface02@mail.ru>
2026-04-21 21:01:30 +0300

    Добавляет обработку login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,27 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { loginThunk } from '../../services/slices/user';
+import { useDispatch } from '../../services/store';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname: '/' } };
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    dispatch(loginThunk({ email, password })).then((_) => {
+      navigate(from, { replace: true });
+    });
   };
 
   return (
