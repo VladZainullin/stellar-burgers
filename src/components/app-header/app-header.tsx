de1f0f92876e72ce8787292e26ad023103e9c273
Vladislav Zainullin <mittoface02@mail.ru>
2026-04-21 21:51:56 +0300

    Добавляет отображение имени пользователя в header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,10 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../services/store';
+import userSlice from '../../services/slices/user';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const userName = useSelector(userSlice.selectors.getName);
+
+  return <AppHeaderUI userName={userName} />;
+};
