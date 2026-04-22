e5afb16823b2ec955df1df3ef764e77e9b8c0e7a
Vladislav Zainullin <mittoface02@mail.ru>
2026-04-22 21:37:08 +0300

    Дорабатывает страницу заказов пользователя

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,20 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import userOrdersSlice, {
+  getUserOrdersThunk
+} from '../../services/slices/userOrders';
+import { useDispatch } from '../../services/store';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+
+  const orders: TOrder[] = useSelector(userOrdersSlice.selectors.getOrders);
+
+  useEffect(() => {
+    dispatch(getUserOrdersThunk());
+  }, [dispatch]);
 
   return <ProfileOrdersUI orders={orders} />;
 };
