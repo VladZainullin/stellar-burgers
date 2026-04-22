e73de9d805bcbf685e54814f3268597876c47155
Vladislav Zainullin <mittoface02@mail.ru>
2026-04-22 22:21:25 +0300

    Добавляет slice для конструктора

---
+++ src/services/slices/constructor/index.ts
@@ -0,0 +1,37 @@
+import { IConstructorState } from './type';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TIngredient } from '@utils-types';
+import { randomUUID } from 'node:crypto';
+
+const initialState: IConstructorState = {
+  bun: null,
+  ingredients: []
+};
+
+const constructorSlice = createSlice({
+  name: 'constructor',
+  initialState: initialState,
+  reducers: {
+    setBun(state, action: PayloadAction<TIngredient>) {
+      if (action.payload.type !== 'bun') return;
+
+      state.bun = action.payload;
+    },
+    add(state, action: PayloadAction<TIngredient>) {
+      if (action.payload.type === 'bun') return;
+
+      action.payload._id = randomUUID();
+      state.ingredients.push(action.payload);
+    },
+    remove(state, action: PayloadAction<TIngredient>) {
+      state.ingredients.splice(state.ingredients.indexOf(action.payload), 1);
+    },
+    reset(state) {
+      state.ingredients = [];
+      state.bun = null;
+    }
+  },
+  selectors: {}
+});
+
+export default constructorSlice;
---
+++ src/services/slices/constructor/type.ts
@@ -0,0 +1,6 @@
+import { TIngredient } from '@utils-types';
+
+export interface IConstructorState {
+  bun: TIngredient | null;
+  ingredients: TIngredient[];
+}
