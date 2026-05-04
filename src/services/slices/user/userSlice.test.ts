27af91ec026c35a91d492f2ff238c04c3efed608
Vladislav Zainullin <mittoface02@mail.ru>
2026-05-04 21:38:20 +0300

    Добавляет тесты получения заказов пользователя

--- src/services/slices/user/userSlice.test.ts
+++ src/services/slices/user/userSlice.test.ts
@@ -1,13 +1,13 @@
 import { TLoginData, TRegisterData } from '@api';
 import userSlice, {
+  getUserOrdersThunk,
   getUserThunk,
   initialState,
   loginThunk,
   logoutThunk,
   registerThunk
 } from './index';
-import { TUser } from '@utils-types';
-import User from './index';
+import { TOrder, TUser } from '@utils-types';
 
 const registerDataMock: TRegisterData = {
   email: 'user',
@@ -199,6 +199,64 @@ describe('Тесты слайса пользователя', () => {
       // Assert
       expect(state.getUserLoading).toBe(false);
       expect(state.getUserError?.message).toEqual(error.message);
+    });
+  });
+
+  describe('Тестирование асинхронного получения заказов пользователя', () => {
+    test('В состоянии <Pending>', () => {
+      // Arrange
+      const action = getUserOrdersThunk.pending('');
+
+      // Act
+      const state = userSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.getOrdersLoading).toBe(true);
+      expect(state.getOrdersError).toBeNull();
+      expect(state.orders).toHaveLength(0);
+    });
+
+    test('В состоянии <Fullfilled>', () => {
+      const ordersMockData: TOrder[] = [
+        {
+          ingredients: [
+            '9733a9a5c3f9b9001cfa093d',
+            '9733a9a5c3f7b9051cfa0941',
+            '9733a9a5c3f7b9101cfa093d'
+          ],
+          _id: '98321a23450ee4ee1d09c6b5',
+          status: 'done',
+          name: 'Тестовый заказ',
+          createdAt: '2026-05-04T21:28:53.041Z',
+          updatedAt: '2026-05-04T21:28:58.011Z',
+          number: 129765
+        }
+      ];
+
+      // Arrange
+      const action = getUserOrdersThunk.fulfilled(ordersMockData, '');
+
+      // Act
+      const state = userSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.getOrdersLoading).toBe(false);
+      expect(state.getOrdersError).toBeNull();
+      expect(state.orders).toEqual(ordersMockData);
+    });
+
+    test('В состоянии <Rejected>', () => {
+      // Arrange
+      const error = new Error('Тестовая ошибка');
+      const action = getUserOrdersThunk.rejected(error, '');
+
+      // Act
+      const state = userSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.getOrdersLoading).toBe(false);
+      expect(state.getOrdersError?.message).toEqual(error.message);
+      expect(state.orders).toHaveLength(0);
     });
   });
 });
