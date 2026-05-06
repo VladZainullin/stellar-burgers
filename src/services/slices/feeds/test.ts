6a8f359d17065fd2ba8965680d30c1c488be0c9c
Vladislav Zainullin <mittoface02@mail.ru>
2026-05-06 20:49:13 +0300

    Добавляет тесты асинхронного получения ленты заказов

---
+++ src/services/slices/feeds/test.ts
@@ -0,0 +1,70 @@
+import feedsSlice, { getFeedsThunk } from './index';
+import { initialState } from './index';
+import { TFeedsResponse } from '@api';
+
+const ordersDataMock: TFeedsResponse = {
+  success: true,
+  orders: [
+    {
+      _id: '844a6105c3D7580216fa083c',
+      status: 'done',
+      name: 'Татарский заказ',
+      createdAt: '',
+      updatedAt: '',
+      number: 1,
+      ingredients: ['654a6505c3D7580216fa083c', '944a6105c3f7580216fa083c']
+    }
+  ],
+  total: 1,
+  totalToday: 1
+};
+
+describe('Тестирование слайса ленты заказов', () => {
+  describe('Тестирование асинхронного получения ленты заказов', () => {
+    test('В состоянии <Pending>', () => {
+      // Arrange
+      const action = getFeedsThunk.pending('');
+
+      // Act
+      const state = feedsSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.orders).toHaveLength(0);
+      expect(state.total).toBe(0);
+      expect(state.totalToday).toBe(0);
+      expect(state.getOrdersLoading).toBeTruthy();
+      expect(state.getOrdersError).toBeNull();
+    });
+
+    test('В состоянии <Fullfilled>', () => {
+      // Arrange
+      const action = getFeedsThunk.fulfilled(ordersDataMock, '');
+
+      // Act
+      const state = feedsSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.orders).toEqual(ordersDataMock.orders);
+      expect(state.total).toEqual(ordersDataMock.total);
+      expect(state.totalToday).toEqual(ordersDataMock.totalToday);
+      expect(state.getOrdersLoading).toBeFalsy();
+      expect(state.getOrdersError).toBeNull();
+    });
+
+    test('В состоянии <Rejected>', () => {
+      // Arrange
+      const error = new Error('Тестовая ошибка');
+      const action = getFeedsThunk.rejected(error, '');
+
+      // Act
+      const state = feedsSlice.reducer(initialState, action);
+
+      // Assert
+      expect(state.orders).toHaveLength(0);
+      expect(state.total).toBe(0);
+      expect(state.totalToday).toBe(0);
+      expect(state.getOrdersLoading).toBeFalsy();
+      expect(state.getOrdersError?.message).toEqual(error.message);
+    });
+  });
+});
