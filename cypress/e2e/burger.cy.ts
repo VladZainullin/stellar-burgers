d15e2ff8d078afe3383b7c23e1499ac250b320fd
Vladislav Zainullin <mittoface02@mail.ru>
2026-05-07 21:09:21 +0300

    Добавляет тесты проверки работоспособности модальных окон

--- cypress/e2e/burger.cy.ts
+++ cypress/e2e/burger.cy.ts
@@ -15,6 +15,44 @@ describe('Тестирование страницы конструктора б�
     ).should('have.length.at.least', 1);
   });
 
+  describe('Тестирование работы модальных окон описаний ингредиентов', () => {
+    describe('Тестирование открытия модальных окон', () => {
+      it('Тестирование открытие по карточке ингредиента', () => {
+        cy.get('[data-ingredient-type="bun"]:first-of-type').click();
+        cy.get('#modals').children().should('have.length', 2);
+      });
+
+      it('Тестирования модального окна после перезагрузки страницы', () => {
+        cy.get('[data-ingredient-type="bun"]:first-of-type').click();
+        cy.reload(true);
+        cy.get('#modals').children().should('have.length', 2);
+      });
+    });
+
+    describe('Тестирование закрытия модальных окон', () => {
+      it('Тестирование закрытия модального окна по кнопке закрыть', () => {
+        cy.get('[data-ingredient-type="bun"]:first-of-type').click();
+        cy.get('#modals button:first-of-type').click();
+        cy.wait(1000);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+
+      it('Тестирование закрытия модального окна по клику на оверлей', () => {
+        cy.get('[data-ingredient-type="bun"]:first-of-type').click();
+        cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+        cy.wait(1000);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+
+      it('Тестирование закрытия модального окна при нажатии клавиши "esc"', () => {
+        cy.get('[data-ingredient-type="bun"]:first-of-type').click();
+        cy.get('body').type('{esc}');
+        cy.wait(1000);
+        cy.get('#modals').children().should('have.length', 0);
+      });
+    });
+  });
+
   afterEach(() => {
     cy.clearCookie('accessToken');
     localStorage.removeItem('refreshToken');
