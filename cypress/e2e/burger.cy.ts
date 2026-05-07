b07b2395c2db208d47b74b0bddcc15763bd915f3
Vladislav Zainullin <mittoface02@mail.ru>
2026-05-07 20:42:22 +0300

    Добавляет тест проверки доступности приложения и тест проверки доступности списка ингредиентов для выбора

--- cypress/e2e/burger.cy.ts
+++ cypress/e2e/burger.cy.ts
@@ -3,4 +3,20 @@ describe('Тестирование страницы конструктора б�
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
     cy.visit('/');
   });
+
+  it('Тестирование доступности приложения', () => {
+    cy.visit('http://localhost:4000');
+  });
+
+  it('Тестирование доступности списка ингредиентов для выбора', () => {
+    cy.get('[data-ingredient-type="bun"]').should('have.length.at.least', 1);
+    cy.get(
+      '[data-ingredient-type="sauce"],[data-ingredient-type="main"]'
+    ).should('have.length.at.least', 1);
+  });
+
+  afterEach(() => {
+    cy.clearCookie('accessToken');
+    localStorage.removeItem('refreshToken');
+  });
 });
